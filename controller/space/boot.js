afb28be2337e1925afc15b95a137905f2ae1dc61
Salvatore Garbesi <sal@dolox.com>
2016-04-30 20:22:28 -0400

    Cleaning code.

--- controller/space/boot.js
+++ controller/space/boot.js
@@ -48,42 +48,78 @@ module.exports = function() {
 		// @todo ok so this we need to fetch when the space changes apparently...
 		var resolution = _.screenResolution();
 
-
-		var windowHeight = Math.floor(resolution.height / input.row.max);
-
-		var windowWidth = Math.floor(resolution.width / input.column.max);
-
 		// Iterate through each of the windows and open them on the specific space.
-		input.window.forEach(function(value, index) {
-			// @todo if application is missing throw a error
+		input.window.forEach(function(value) {
+			// If the `application` property is missing, then skip the iteration.
+			if (_.isEmpty(value.application) === true) {
+				// Throw a error to console.
+				log.error('No application name specified for window! Skipping.', value);
 
-			if (column < input.column.max - 1) {
-				column++;
+				// Skip the iteration..
+				return;
 			}
 
-			
-			else {
-				row++;
-				column = 0;
-			}
+			var windowHeight = 0;
+			var windowWidth = 0;
+			var windowX = 0;
+			var windowY = 0;
 
-			var left = (column * (resolution.width / input.column.max));
-			var top = (row * (resolution.height / input.row.max));
+			// Process the grid if the maximum number of columns/rows aren't set to `0`.
+			if (input.column.max > 0 && input.row.max > 0) {
 
-			if (input.column.max > 1 && column == input.column.max - 1 && input.column.spacing) {
-				left += input.column.spacing;
-			}
 
-			if (input.row.max > 1 && row == input.row.max - 1 && input.row.spacing) {
-				top += input.row.spacing;
-			}
+				// @todo
+				windowHeight = Math.floor(resolution.height / input.row.max);
 
-			if (input.row.max > 1) {
-				windowHeight -= input.row.spacing;
-			}
+				windowWidth = Math.floor(resolution.width / input.column.max);
 
-			if (input.column.max > 1) {
-				windowWidth -= input.column.spacing;
+
+
+				// Increment the column before the row.
+				if (column < input.column.max - 1) {
+					column++;
+				}
+
+				// If the columns have exhausted, then increment the row.
+				else {
+					// Increment the row.
+					row++;
+
+					// Reset the column.
+					column = 0;
+				}
+
+				// Adjust the X position of the window.
+				windowX = column * (resolution.width / input.column.max);
+
+				// Adjust the Y position of the window.
+				windowY = row * (resolution.height / input.row.max);
+
+				// If column spacing is enabled, then adjust the window.
+				if (input.column.spacing > 0 && input.column.max > 1) {
+					// Only adjust the X position, if it's not the first column.
+					if (column > 1) {
+						windowX += input.column.spacing;
+					}
+
+					// Adjust the window width.
+					windowWidth -= input.column.spacing;
+				}
+
+				// If row spacing is enabled, then adjust the window.
+				if (input.row.spacing > 0 && input.row.max > 1) {
+					// Only adjust the Y position, if it's not the first row.
+					if (row > 1) {
+						windowY += input.row.spacing;
+					}
+
+					// Adjust the window height.
+					windowHeight -= input.row.spacing;
+				}
+
+				if (input.column.max > 1) {
+					windowWidth -= input.column.spacing;
+				}
 			}
 
 			// Append a new line.
@@ -114,21 +150,27 @@ module.exports = function() {
 				value.shellPost.join(';'),
 				windowHeight,
 				windowWidth,
-				left,
-				top
+				windowX,
+				windowY
 			]);
 		});
 
-		var d = path.join(instanceDirectory, 'window.applescript');
-		fs.writeFileSync(d, appleScript);
+		var appleScriptFile = path.join(instanceDirectory, 'window.applescript');
+		fs.writeFileSync(appleScriptFile, appleScript);
 
+		// Attempt to invoke the batch script.
 		try {
-			_.exec('osascript ' + d);
-		} catch (e) {
-			log.error('command failed');
+			// Invoke the AppleScript.
+			_.exec('osascript ' + appleScriptFile);
+		}
+
+		// Catch any exceptions.
+		catch (exception) {
+			// Throw a error to console.
+			log.error('Building of space failed.', exception);
 		}
 
 		// Remove the temporary directory.
-		//_.exec('rm -r ' + instanceDirectory);
+		_.exec('rm -r ' + instanceDirectory);
 	};
 };
